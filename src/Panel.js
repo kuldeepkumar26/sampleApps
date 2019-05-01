b7dbc717484172eb2628f63737d5b06152d70bcf
kuldeep <kuldeep@triconinfotech.com>
2019-05-01 23:57:52 +0530

    midnight

--- src/Panel.js
+++ src/Panel.js
@@ -1,31 +1,133 @@
 import React, {Component} from 'react';
-import panelCss from './Panel.css';
+import css from './Panel.css';
+import BabyGif from './baby.gif';
+//import fetch from 'fetch';
 
 class Panel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            data: null
+        }
+    }
+    componentWillMount() {
+    fetch('http://www.healtheoz.co.in/sharedData/JOawDvc73WgxRtMkH93xhDBKYh0TZkKrb4PmpydY')
+        .then((response)=> {
+            return response.json();
+        })
+        .then((myJson)=> {
+            console.log("INSIDE",JSON.stringify(myJson));
+            let data;
+            try {
+                data = myJson.sharedData.diary
+            } catch(ex) {
+                data = []
+            }
+            this.setState({
+                isLoading: false,
+                data: data
+            })
+        });
+    }
     render() {
+        var child = null;
+        if(this.state.isLoading) {
+            child = <div>loading..................</div>
+        } else {
+            child =  <PanelItems data={this.state.data}></PanelItems>
+        }
         return (
-            <div id="allDiaryDiv" className="panel">
-                <div>
-                    <div className="panel-body" id="panelBody" >
-                        <div className="fb-user-thumb">
-                            <img src="./baby.gif" alt=""/>
-                        </div>
-                        <div className="fb-user-details">
-                            <h3 id="title">nxnxbj</h3>
-                            <p id="date">Tue Mar 19 2019</p>
-                            <audio src="https://s3-ap-southeast-1.amazonaws.com/walkon-images-dev/1552999464144.x-wav" controls="controls"></audio>
-                        </div>
-                        <div className="clearfix"></div>
-                        <div>
-                            <img id="image"/>
-                            <p className="fb-user-status" id="message">ndndnd</p>
-                        </div>
-                    </div>
-                </div>
+            <div id="allDiaryDiv" className={css.panel}>
+            {child}
             </div>
-                    
         )
     }
 }
 
+class PanelItems extends Component {
+    constructor(props) {
+        super(props);
+        this.checkMediaType = this.checkMediaType.bind(this);
+        this.getExtension = this.getExtension.bind(this);
+        this.isAudio = this.isAudio.bind(this);
+        this.isImage = this.isImage.bind(this);
+    }
+    render() {
+        const data = this.props.data;
+        const toShow = data.map((element)=> {
+            var elem = null;
+            if(element.mediaURL) {
+                var typeOfMedia = this.checkMediaType(element.mediaURL);
+                console.log("RRRRRRRR", typeOfMedia)
+                if (typeOfMedia === 2) {
+                    elem = <audio src={element.mediaURL} controls="controls"></audio>
+                } else {
+                    elem = <img id="imageInPanel" src={element.mediaURL} alt=""/>
+                }
+              } 
+            return (
+                <div key={element.id}> 
+                <div  className={css.panelBody} id="panelBody" >
+                <div className={css.fbUserThumb}>
+                    <img src={BabyGif} alt=""/>
+                </div>
+                <div className={css.fbUserDetails}>
+                    <h3 id="title">{element.title}</h3>
+                    <p id="date">{element.dated}</p>
+                    {elem}
+                </div>
+                <div className={css.clearfix}></div>
+                <div>
+                    <img id="image" alt=""/>
+                    <p className={css.fbUserStatus} id="message">{element.message}</p>
+                </div>
+            </div>
+            </div>
+            )
+        })
+        return toShow;
+    }
+
+    checkMediaType(file) {
+		var fileIsImage = this.isImage(file);
+		if (fileIsImage) {
+			return 1;
+		}
+		var fileIsAudio = this.isAudio(file);
+		if (fileIsAudio) {
+			return 2;
+		}
+	}
+	getExtension(filename) {
+		var parts = filename.split('.');
+		return parts[parts.length - 1];
+	}
+
+	isImage(filename) {
+		var ext = this.getExtension(filename);
+		switch (ext.toLowerCase()) {
+			case 'jpg':
+			case 'jpeg':
+			case 'gif':
+			case 'bmp':
+            case 'png':
+                return true;
+            default:
+				return false;
+		}
+	}
+
+	isAudio(filename) {
+		var ext = this.getExtension(filename);
+		switch (ext.toLowerCase()) {
+			case 'x-wav':
+			case 'wav':
+				// etc
+                return true;
+            default:
+                return false;
+		}
+	}
+} 
 export default Panel;
